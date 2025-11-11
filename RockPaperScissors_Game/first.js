162a003fc58d126827feab6745263dc434bcf6bf
priyankarajput8758-cpu <priyankarajput8758@gmail.com>
2025-11-11 22:29:21 +0530

    Create first.js

---
+++ RockPaperScissors_Game/first.js
@@ -0,0 +1,65 @@
+let userscore=0;
+let compscore=0;
+
+const choices= document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+
+const userscorepara=document.querySelector("#user-score");
+const compscorepara=document.querySelector("#comp-score");
+
+const gencompchoice=()=>{
+    const options=["rock","paper","scissors"];
+    const randidx=Math.floor(Math.random()*3);
+    return options[randidx];
+};
+const drawgame=()=>{
+   
+    msg.innerText="draw";
+};
+
+const showwinner=(userwin,userchoice,compchoice)=>{
+    if(userwin){
+        userscore++;
+        userscorepara.innerText=userscore;
+        msg.innerText=`you win! ${userchoice} beats ${compchoice}`;
+        msg.style.backgroundColor="green";
+    }
+    else{
+        compscore++;
+        compscorepara.innerText=compscore;
+        
+        msg.innerText=`you lost! ${compchoice} beats ${userchoice}`;
+        msg.style.backgroundColor="red";
+    }
+}
+const playgame = (userchoice) =>{
+   
+
+    //generate computer choice 
+    const compchoice=gencompchoice();
+    
+    //draw game
+    if (userchoice==compchoice){
+        drawgame();
+    }
+    else{
+        let userwin=true;
+        if (userchoice==="rock"){
+            userwin= compchoice==="scissors"? true:false;
+        }
+        else if (userchoice==="paper"){
+            userwin=compchoice=="rock"? true:false;
+        }
+        else{
+            userwin=compchoice==="paper"?true:false;
+        }
+        showwinner(userwin,userchoice,compchoice);
+    }
+
+};
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userchoice=choice.getAttribute("id");
+        playgame(userchoice);
+    });
+});
